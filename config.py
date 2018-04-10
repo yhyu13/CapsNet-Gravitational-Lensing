8c3703165a5229cc273a5054739ccf2a0d5ed60e
Hang Yu @ UIUC <yohan680919@gmail.com>
2018-04-10 12:11:55 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,59 @@ import argparse
 import os
 from collections import namedtuple
 
+parser = argparse.ArgumentParser(description='Define parameters.')
+
+"""Network hyperparameters"""
+parser.add_argument('--n_epoch', type=int, default=1)
+parser.add_argument('--global_epoch', type=int, default=50)
+parser.add_argument('--n_batch', type=int, default=50)
+parser.add_argument('--n_img_row', type=int, default=192)
+parser.add_argument('--n_img_col', type=int, default=192)
+parser.add_argument('--n_img_channels', type=int, default=1)
+parser.add_argument('--n_classes', type=int, default=10)
+parser.add_argument('--n_labels', type=int, default=5)
+parser.add_argument('--lr', type=float, default=1e-3)
+parser.add_argument('--model', default='cap', help='choose between cnn and cap')
+parser.add_argument('--dataset', dest='processed_dir', default='./MNIST_data')
+parser.add_argument('--load_model_path', dest='load_model_path', default=None)  # './savedmodels'
+parser.add_argument('--mode', dest='MODE', default='train', help='choose between train and test')
+
+FLAGS = parser.parse_args()
+
+"""CapsNet hyperparameters"""
+HParams = namedtuple('HParams',
+                     'batch_size, num_classes, num_labels '
+                     'filters, strides, cnn_kernel_size, padding, '
+                     'lambda_margin_loss,m_plus_margin_loss,m_minus_margin_loss, '
+                     'num_routing, standard, label_masking, grad_summary, grad_defense, '
+                     'weight_decay_rate, relu_leakiness, '
+                     'fixed_lrn, min_lrn_rate, lrn_rate, decay_step, optimizer, temperature, global_norm, ')
+
+HPS = HParams(batch_size=FLAGS.n_batch,
+              num_classes=FLAGS.n_classes,
+              num_labels=FLAGS.n_labels,
+              filters=[1, 128, 16, 16],
+              strides=[5, 5],
+              cnn_kernel_size=9,
+              padding="VALID",
+              lambda_margin_loss=0.5,
+              m_plus_margin_loss=0.9,
+              m_minus_margin_loss=0.1,
+              num_routing=3,
+              standard=True,
+              label_masking=True,
+              grad_summary=False,
+              grad_defense=False,
+              weight_decay_rate=1e-4,
+              relu_leakiness=0.0,
+              fixed_lrn=True,
+              min_lrn_rate=1e-5,
+              lrn_rate=1e-3,
+              decay_step=100,
+              optimizer='adam',
+              temperature=1.0,
+              global_norm=100)
+
 # https://github.com/yasharhezaveh/Ensai/blob/master/init.py
 '''
    Defining global variables
@@ -64,56 +117,3 @@ train_log_folder = cwd + '/train_log'
 test_log_folder = cwd + '/test_log'
 
 savedmodel_path = cwd + '/savedmodels/'
-
-parser = argparse.ArgumentParser(description='Define parameters.')
-
-"""Network hyperparameters"""
-parser.add_argument('--n_epoch', type=int, default=1)
-parser.add_argument('--global_epoch', type=int, default=50)
-parser.add_argument('--n_batch', type=int, default=50)
-parser.add_argument('--n_img_row', type=int, default=192)
-parser.add_argument('--n_img_col', type=int, default=192)
-parser.add_argument('--n_img_channels', type=int, default=1)
-parser.add_argument('--n_classes', type=int, default=10)
-parser.add_argument('--n_labels', type=int, default=5)
-parser.add_argument('--lr', type=float, default=1e-3)
-parser.add_argument('--model', default='cap', help='choose between cnn and cap')
-parser.add_argument('--dataset', dest='processed_dir', default='./MNIST_data')
-parser.add_argument('--load_model_path', dest='load_model_path', default=None)  # './savedmodels'
-parser.add_argument('--mode', dest='MODE', default='train', help='choose between train and test')
-
-FLAGS = parser.parse_args()
-
-"""CapsNet hyperparameters"""
-HParams = namedtuple('HParams',
-                     'batch_size, num_classes, num_labels '
-                     'filters, strides, cnn_kernel_size, padding, '
-                     'lambda_margin_loss,m_plus_margin_loss,m_minus_margin_loss, '
-                     'num_routing, standard, label_masking, grad_summary, grad_defense, '
-                     'weight_decay_rate, relu_leakiness, '
-                     'fixed_lrn, min_lrn_rate, lrn_rate, decay_step, optimizer, temperature, global_norm, ')
-
-HPS = HParams(batch_size=FLAGS.n_batch,
-              num_classes=FLAGS.n_classes,
-              num_labels=FLAGS.n_labels,
-              filters=[1, 128, 16, 16],
-              strides=[5, 5],
-              cnn_kernel_size=9,
-              padding="VALID",
-              lambda_margin_loss=0.5,
-              m_plus_margin_loss=0.9,
-              m_minus_margin_loss=0.1,
-              num_routing=3,
-              standard=True,
-              label_masking=True,
-              grad_summary=False,
-              grad_defense=False,
-              weight_decay_rate=1e-4,
-              relu_leakiness=0.0,
-              fixed_lrn=True,
-              min_lrn_rate=1e-5,
-              lrn_rate=1e-3,
-              decay_step=100,
-              optimizer='adam',
-              temperature=1.0,
-              global_norm=100)
